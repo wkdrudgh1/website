7121e585291d9fb0fc7c8b244b4a9d99fa7e2d1f
wkdrudgh1 <wkdrudgh1@naver.com>
2022-09-14 10:24:43 +0900

    전체디자인 완성

--- app.js
+++ app.js
@@ -1,7 +1,56 @@
-const menu = document.querySelector('#mobile-menu')
-const menuLinks = document.querySelector('.navbar__menu')
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar__menu');
+const navLogo = document.querySelector('#navbar__logo');
 
-menu.addEventListener('click', function() {
+// Display Mobile Menu
+const MobileMenu = () => {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
-})
+};
+
+menu.addEventListener('click', MobileMenu);
+
+
+// Show active menu when scrolling
+const highlightMenu = () => {
+    const elem = document.querySelector('.highlight');
+    const homeMenu = document.querySelector('#home-page');
+    const aboutMenu = document.querySelector('#about-page');
+    const servicesMenu = document.querySelector('#services-page');
+    let scrollPos = window.scrollY;
+    // console.log(scrollPos);
+    // adds 'highlight' class to my menu items
+    if(window.innerWidth > 960 && scrollPos < 600) {
+        homeMenu.classList.add('highlight');
+        aboutMenu.classList.remove('highlight');
+        return;
+    } else if (window.innerWidth > 960 && scrollPos < 1400) {
+        aboutMenu.classList.add('highlight');
+        homeMenu.classList.remove('highlight');
+        servicesMenu.classList.remove('highlight');
+        return;
+    } else if (window.innerWidth > 960 && scrollPos < 2345) {
+        servicesMenu.classList.add('highlight');
+        aboutMenu.classList.remove('highlight');
+        return;
+    }
+    
+    if((elem && window.innerWidth < 960 && scrollPos < 600) || elem) {
+        elem.classList.remove('highlight');
+    }
+}
+
+window.addEventListener('scroll', highlightMenu)
+window.addEventListener('click', highlightMenu)
+
+// Clase mobile Menu whem clicking a menu item
+const hidemobileMenu = () => {
+    const menuBards = document.querySelector('is-active');
+    if(window.innerWidth <= 768 && menuBards) {
+        menu.classList.toggle('is-active');
+        menuLinks.classList.remove('active');
+    }
+};
+
+menuLinks.addEventListener('click', hidemobileMenu);
+navLogo.addEventListener('click',hidemobileMenu);
